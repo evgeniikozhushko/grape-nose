cf49a6a7ee86fedfb492b720a668656128abed6d
evgeniikozhushko <climbersrock.eugene@gmail.com>
2025-06-01 14:05:18 -0600

    Button updated

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ export default function App() {
               <Instagram className="w-3 h-3" />
               <span>thegrapenose</span>
             </a>
-            <Button  className="bg-gradient-to-tr from-grape-red to-red-400 text-white shadow-lg my-4" radius="sm" variant="ghost">
+            <Button  className="bg-gradient-to-tr from-grape-red to-red-400 text-white shadow-lg my-4" radius="sm" >
               <a
                 href="mailto:thegrapenose@gmail.com"
               >
